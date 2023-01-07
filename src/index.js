587473dd1405b9c135fafae8ff57215cd7bf8357
Shreyaar12 <shreyarajpal6@gmail.com>
2023-01-07 22:22:31 +0530

    search added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 // import React from "react";
-// import ReactDOM from "react-dom";
+import ReactDOM from "react-dom";
 // import "./index.css";
 // import App from "./App";
 
